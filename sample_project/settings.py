2ed7b09688f888ac916f30c0f87dc3082214cbf4
administrator <administrator@charette10.ing.puc.cl>
2018-09-11 00:49:44 -0300

    ready to deploy with nginx

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qrz(0*c2^ci#v%2u=#x($p&5ze%7s**n+rpy%v&1o9p@g7@)bw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['charette10.ing.puc.cl']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
